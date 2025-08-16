07cbd1564f8351c6eb673e6e2d77c5f034c71019
qJulio <111520530+qJulio@users.noreply.github.com>
2025-08-15 19:30:38 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const modal=$("#detalleModal");
 const formatMoney=v=>(v>=10000?`$${v.toLocaleString()}`:`$${v}`);
 
 // ==== URL de tu Google Sheets Web App ====
-const SHEETS_URL = "TU_WEB_APP_URL_AQUI";
+const SHEETS_URL = "https://script.google.com/macros/s/AKfycbxAylI_RZchCNT5Igb9i9uJCS6zdUPnlc3PoNcXSAItL5rSKimNaXT2MwLFHes97v3Q/exec";
 
 // ==== Cargar propiedades desde Google Sheets ====
 async function loadPropsFromSheet(){
